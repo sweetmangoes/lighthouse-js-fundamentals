1edee1884ae512e850706092403e510421171993
sweetmangoes <alphonsechristian@gmail.com>
2022-09-04 13:25:40 -0400

    adding odd.js

---
+++ odd.js
@@ -0,0 +1,12 @@
+//This function should take a number as a parameter and return true if it is odd and false otherwise.
+
+function isOdd (num) {
+if (num % 2 === 0 ) {
+  return false;
+  } else {
+    return true;
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
